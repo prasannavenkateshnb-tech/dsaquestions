21d814b86895540a047bc2a987bd06393279a75e
Prasanna <prasannavenkatesh070606@gmail.com>
2025-10-24 20:04:11 +0530

    logic for digits count

---
+++ Digits.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Digits {
+    public int countDigits(int num){
+        int counter=0;
+        while(num>0){
+            int lastDigit=num%10;
+            counter++;
+            num=num/10;
+        }
+        return counter;
+
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int num=sc.nextInt();
+        Digits obj=new Digits();
+        int result=obj.countDigits(num);
+        System.out.println("The total digits in the number is "+result);
+
+    }
+    
+}
