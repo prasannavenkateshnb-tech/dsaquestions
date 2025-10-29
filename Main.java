af505f37f1bb2adc7fd0b12b07bce4c6aad0a90b
Prasanna <prasannavenkatesh070606@gmail.com>
2025-10-29 14:00:31 +0530

    logive for rearranging pos and neg

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import java.util.*;
 class Main{
-    public int[] moveZeros(int[] arr){
-        int i=0;
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]!=0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                i++;
+    public int[] rearrangeArray( int[] arr){
+        int[] nums=new int[arr.length];
+        int pos=0;
+        int neg=1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                nums[pos]=arr[i];
+                pos+=2;
+            }
+            else{
+                nums[neg]=arr[i];
+                neg+=2;
             }
         }
-        return arr;
+        return nums;
+
 
 
     }
@@ -19,14 +24,14 @@ class Main{
         System.out.println("Enter the number of elements in an array ");
         int N=sc.nextInt();
         int[] arr=new int[N];
-        System.out.println("Enter the array elements consisting zeros ");
+        System.out.println("Enter the array elements consisting of positive and negatives ");
         for(int i=0;i<N;i++){
             arr[i]=sc.nextInt();
 
         }
         Main obj =new Main();
-        int[] result=obj.moveZeros(arr);
-        System.out.println("after moving zeros "+Arrays.toString(result));
+        int[] result=obj.rearrangeArray(arr);
+        System.out.println("After rearranging positive and negative alternatively "+Arrays.toString(result));
 
     }
 }
