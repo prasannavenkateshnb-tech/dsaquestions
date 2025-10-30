8d9b9acab23491e10423005616ed905495ca4337
Prasanna <prasannavenkatesh070606@gmail.com>
2025-10-30 18:05:31 +0530

    logic

--- Main.java
+++ Main.java
@@ -1,37 +1,32 @@
 import java.util.*;
 class Main{
-    public int[] rearrangeArray( int[] arr){
-        int[] nums=new int[arr.length];
-        int pos=0;
-        int neg=1;
+    public void countEvenOdd(int[] arr){
+        int oddCounter=0;
+        int evenCounter=0;
+
         for(int i=0;i<arr.length;i++){
-            if(arr[i]>0){
-                nums[pos]=arr[i];
-                pos+=2;
+            if(arr[i]%2==0){
+                evenCounter++;
             }
             else{
-                nums[neg]=arr[i];
-                neg+=2;
+                oddCounter++;
             }
         }
-        return nums;
-
-
-
+        System.out.println("The odd numbers count= "+oddCounter);
+        System.out.println("The even numbers count= "+evenCounter);
     }
+
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of elements in an array ");
-        int N=sc.nextInt();
+        System.out.println("Enter the number of array elements ");
+        int N =sc.nextInt();
         int[] arr=new int[N];
-        System.out.println("Enter the array elements consisting of positive and negatives ");
+        System.out.println("Enter the  array elements ");
         for(int i=0;i<N;i++){
             arr[i]=sc.nextInt();
-
         }
-        Main obj =new Main();
-        int[] result=obj.rearrangeArray(arr);
-        System.out.println("After rearranging positive and negative alternatively "+Arrays.toString(result));
+        Main obj=new Main();
+        obj.countEvenOdd(arr);
 
     }
 }
