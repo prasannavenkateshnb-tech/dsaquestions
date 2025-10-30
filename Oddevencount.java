3342fe1394b121ee6321d2b2ccc310ea083bc5e2
Prasanna <prasannavenkatesh070606@gmail.com>
2025-10-30 18:07:06 +0530

    logic for odd even count

---
+++ Oddevencount.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Oddevencount{
+    public void countEvenOdd(int[] arr){
+        int oddCounter=0;
+        int evenCounter=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                evenCounter++;
+            }
+            else{
+                oddCounter++;
+            }
+        }
+        System.out.println("The odd numbers count= "+oddCounter);
+        System.out.println("The even numbers count= "+evenCounter);
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of array elements ");
+        int N =sc.nextInt();
+        int[] arr=new int[N];
+        System.out.println("Enter the  array elements ");
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+        Oddevencount obj=new Oddevencount();
+        obj.countEvenOdd(arr);
+
+    }
+}
